dca28204a39d053203c432b7f5465344aa21c3a2
JeongJunSeo99 <wnstj0810@naver.com>
2024-11-05 11:28:44 +0900

    github actions 활용

--- src/main/java/com/swcamp9th/eb_proj/service/MenuService.java
+++ src/main/java/com/swcamp9th/eb_proj/service/MenuService.java
@@ -1,8 +1,24 @@
 package com.swcamp9th.eb_proj.service;
 
+import com.swcamp9th.eb_proj.dto.MenuDTO;
+import com.swcamp9th.eb_proj.repository.MenuRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MenuService {
 
+    private final MenuRepository menuRepository;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public MenuService(MenuRepository menuRepository, ModelMapper modelMapper) {
+        this.menuRepository = menuRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public MenuDTO findByMenuCode(int menuCode) {
+        return modelMapper.map(menuRepository.findById(menuCode), MenuDTO.class);
+    }
 }
