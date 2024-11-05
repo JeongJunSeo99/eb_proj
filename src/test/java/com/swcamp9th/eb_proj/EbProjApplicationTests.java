8788232ed7c816e460ac276f58dde8fbbed3f12a
JeongJunSeo99 <wnstj0810@naver.com>
2024-11-05 09:28:02 +0900

    first commit

---
+++ src/test/java/com/swcamp9th/eb_proj/EbProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swcamp9th.eb_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
